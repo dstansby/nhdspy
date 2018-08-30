29100631cf1c4713dbfc55661672922190fc9d07
David Stansby <dstansby@gmail.com>
2018-08-30 13:52:27 +0100

    Don't print input file

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 electrons = NHDSPy.Species(-1, 1 / 1836, 1, 0, 1, 1)
 protons = NHDSPy.Species(1, 1, 1, 0, 1, 1)
 input = NHDSPy.InputParams([electrons, protons], 0.009 - 0.0001 * 1j, 0.001, 1e-4)
-print(NHDSPy.format_input_file(input))
 output = NHDSPy.run(input)
 
 fig, ax = plt.subplots()
