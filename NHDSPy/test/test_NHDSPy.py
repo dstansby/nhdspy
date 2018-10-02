d2c66364f2b51afafdc0ae306f830a27a9778105
David Stansby <dstansby@gmail.com>
2018-10-02 12:46:09 +0100

    Update test file

--- NHDSPy/test/test_NHDSPy.py
+++ NHDSPy/test/test_NHDSPy.py
@@ -1,8 +1,8 @@
-import NHDSPy
+import nhdspy
 
 
 def test_smoke():
-    electrons = NHDSPy.Species(-1, 1 / 1836, 1, 0, 1, 1)
-    protons = NHDSPy.Species(1, 1, 1, 0, 1, 1)
-    input = NHDSPy.InputParams([electrons, protons], 0.009 - 0.0001 * 1j, 0.001, 1e-4)
-    output = NHDSPy.run(input)
+    electrons = nhdspy.Species(-1, 1 / 1836, 1, 0, 1, 1)
+    protons = nhdspy.Species(1, 1, 1, 0, 1, 1)
+    input = nhdspy.InputParams([electrons, protons], 0.009 - 0.0001 * 1j, 0.001, 1e-4)
+    output = nhdspy.run(input)
