1f0b805494202dbf72da8462a603ab07d413e59c
David Stansby <dstansby@gmail.com>
2018-12-15 12:39:45 +0000

    Fix smoke test

--- nhdspy/test/test_NHDSPy.py
+++ nhdspy/test/test_NHDSPy.py
@@ -4,5 +4,7 @@ import nhdspy
 def test_smoke():
     electrons = nhdspy.Species(-1, 1 / 1836, 1, 0, 1, 1)
     protons = nhdspy.Species(1, 1, 1, 0, 1, 1)
-    input = nhdspy.InputParams([electrons, protons], 0.009 - 0.0001 * 1j, 0.001, 1e-4)
+    input = nhdspy.InputParams([electrons, protons],
+                               0.009 - 0.0001 * 1j,
+                               0.001, 1e-4, 0.1, 1)
     output = nhdspy.run(input)
